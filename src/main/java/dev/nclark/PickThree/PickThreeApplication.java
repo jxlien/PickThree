2b70a71e1d1a482a00087e36351b23d87c521c9c
Julien Sweat <33526105+jxlien@users.noreply.github.com>
2022-02-25 00:46:07 -0500

    Initial commit

---
+++ src/main/java/dev/nclark/PickThree/PickThreeApplication.java
@@ -0,0 +1,13 @@
+package dev.nclark.PickThree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nclark/PickThree/PickThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nclark.PickThree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
